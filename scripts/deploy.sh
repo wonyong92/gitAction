b2e4525f1dd97aec73d03ae4415d6f7cfeeb6b53
wonyong92 <80381715+wonyong92@users.noreply.github.com>
2022-08-08 18:28:15 +0900

    ss

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -20,7 +20,7 @@ else
   echo "> kill -15 $CURRENT_PID" >> /home/ubuntu/action/deploy.log
   sudo kill -15 $CURRENT_PID
   while pgrep -f $CURRENT_PID > /dev/null; do
-  echo "killing ..."
+  echo "> closing...." >> /home/ubuntu/action/deploy.log
   sleep 1
 done
   sleep 10
