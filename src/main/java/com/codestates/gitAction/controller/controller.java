79ad2bc0c5e62b43c8da8dd1854e5ad54c35ca9a
wonyong92 <80381715+wonyong92@users.noreply.github.com>
2022-08-08 18:59:54 +0900

    Update controller.java

--- src/main/java/com/codestates/gitAction/controller/controller.java
+++ src/main/java/com/codestates/gitAction/controller/controller.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class controller {
+public class controller { 
     @GetMapping("/")
     public String hello(){
         return "helloo11112345678";
