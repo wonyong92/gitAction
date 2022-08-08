7f3cc53e190292672406c632f0bf001b99a5a077
wonyong92 <80381715+wonyong92@users.noreply.github.com>
2022-08-08 11:42:22 +0900

    ddd

---
+++ src/test/java/com/codestates/gitAction/GitActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.gitAction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitActionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
