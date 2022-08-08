8c6a3f25cb39d2905fdd9558e0a76a55e58265a7
wonyong92 <80381715+wonyong92@users.noreply.github.com>
2022-08-08 14:49:10 +0900

    Create controller.java

---
+++ src/main/java/com/codestates/controller/controller.java
@@ -0,0 +1,12 @@
+package com.codestates.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controller {
+    @GetMapping("/")
+    public String hello(){
+        return "helloo";
+    }
+}
